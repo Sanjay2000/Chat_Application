8e508cbaaf14db943d37887ab31aa6f0733ff6a9
Sanjay2000 <8375sanjaykumar@gmail.com>
2020-09-17 10:36:36 +0530

    Done

---
+++ src/utils/messages.js
@@ -0,0 +1,23 @@
+const genrateMessage = (username,text) =>{
+    return {
+        username,
+        text,
+        createdAt: new Date().getTime()
+
+    }
+}
+
+const genrateLocationMessage = (username, url) => {
+    return {
+        username,
+        url,
+        createdAt: new Date().getTime()
+    }
+
+}
+
+
+module.exports = {
+    genrateMessage,
+    genrateLocationMessage
+}
